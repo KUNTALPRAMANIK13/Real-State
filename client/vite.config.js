828e57267008950cd7a10c9a374c4dbe8ff63f55
Kuntal <kuntalpramanik964@gmail.com>
2024-11-07 17:13:07 +0530

    Update API proxy target in Vite configuration to point to production URL

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server:{
     proxy:{
       "/api":{
-        target:'http://localhost:3000',
+        target:'real-state-hsfa.vercel.app',
         secure: false
       },
     },
