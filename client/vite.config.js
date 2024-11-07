70e1203899614d0d28172158bf6fedbbc3193b09
Kuntal <kuntalpramanik964@gmail.com>
2024-11-07 18:45:21 +0530

    Restore API proxy configuration in Vite and remove commented-out CORS options in Express

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      // "/api":{
-      //   target:'http://localhost:3000',
-      //   secure: false
-      // },
+      "/api":{
+        target:'http://localhost:3000',
+        secure: false
+      },
     },
   },
   plugins: [react()],
