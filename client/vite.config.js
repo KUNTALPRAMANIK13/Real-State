d96acbf9403b6ed931ad9760dce2ce57f0e5fc6d
Kuntal <kuntalpramanik964@gmail.com>
2024-11-07 17:23:00 +0530

    Update API proxy target in Vite configuration to use full URL

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server:{
     proxy:{
       "/api":{
-        target:'real-state-hsfa.vercel.app',
+        target:'https://real-state-hsfa.vercel.app',
         secure: false
       },
     },
