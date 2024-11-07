9aa5f423e4cac13f4eb836b2b615a11ccbccb07e
Kuntal <kuntalpramanik964@gmail.com>
2024-11-07 18:12:33 +0530

    Update API proxy target in Vite configuration to use localhost for development

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server:{
     proxy:{
       "/api":{
-        target:'https://real-state-hsfa.vercel.app',
+        target:'http://localhost:3000',
         secure: false
       },
     },
