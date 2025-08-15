4365373d0d0401b16a9db605958debf7329d6c51
Kuntal <kuntalpramanik964@gmail.com>
2025-08-15 14:58:46 +0530

    fix: Update API proxy target to production URL

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server:{
     proxy:{
       "/api":{
-        target:'http://localhost:3000',
+        target:'https://real-state-gray.vercel.app',
         secure: false
       },
     },
