8cd1a59eb45af5d5586677fcbe3a5925442039aa
Kuntal <kuntalpramanik964@gmail.com>
2025-08-15 18:22:31 +0530

    feat: Update authentication middleware in Cloudinary routes to use verifyToken

--- api/routes/cloudinary.route.js
+++ api/routes/cloudinary.route.js
@@ -5,20 +5,18 @@ import {
   getImageDetails,
   cleanupImages,
 } from "../controllers/cloudinary.controller.js";
-import { verifyUser } from "../utils/verifyUser.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 // Delete image - requires authentication
-router.delete("/delete", verifyUser, deleteImage);
+router.delete("/delete", verifyToken, deleteImage);
 
 // Get user images - requires authentication
-router.get("/user-images/:userId", verifyUser, getUserImages);
+router.get("/user-images/:userId", verifyToken, getUserImages);
 
-// Get image details - requires authentication
-router.get("/image/:publicId", verifyUser, getImageDetails);
+// Get image details - requires authentication  
+router.get("/image/:publicId", verifyToken, getImageDetails);
 
 // Cleanup images - admin only (you might want to add admin verification)
-router.post("/cleanup", verifyUser, cleanupImages);
-
-export default router;
+router.post("/cleanup", verifyToken, cleanupImages);export default router;
